9cfd0fee87579f700782cfa71342150e8cc31210
Divyesh Nandlal Vishwakarma <divyesh1099@gmail.com>
2021-04-11 14:35:08 +0530

    Main Logic Left, basic templates done. Frontend Remaining

--- cyber_bully_website/urls.py
+++ cyber_bully_website/urls.py
@@ -15,12 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+# Static and media imports
 from django.conf import settings
 from django.conf.urls.static import static
 
 # Url Patterns Here
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profanity_text/', include('profanity_text.urls'))
+    path('profanity_text/', include('profanity_text.urls')),
+    path('profanity_text_api/', include('profanity_text_api.urls'))
 ] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+
+if settings.DEBUG :
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
