c97c92a334db2135c950074c3c01e962bcd0bbec
mruga7 <mrugakshi9@gmail.com>
2021-04-18 12:31:35 +0530

    logic changed

--- profanity_text/views.py
+++ profanity_text/views.py
@@ -2,42 +2,47 @@ from django.shortcuts import render
 from .models import Cusslist
 import datetime
 
-# Our Main Logic
 def convert_cuss_word_to_stars(a_word):
-	final_starred_word ="*"*len(a_word)
-	return final_starred_word
+    final_starred_word = "*" * len(a_word)
+    return final_starred_word
 
-def check_special_character(a_string, a_list):
+def check_for_special_character(a_word):
+    for j in a_word:
+        if j.isalpha()==False:
+            return True
+
+
+def check_if_actually_a_cussword(a_word,cuss_words_list):
+    thatword=""
+    for k in a_word:
+        if k.isalpha()==True:
+                thatword+=k
+    if thatword in a_word and thatword in cuss_words_list:
+        return True
+    else:
+        return False
+
+
+
+def check_special_character(a_string, cuss_words_list):
     list_of_words_from_string = a_string.split()
     for a_word in list_of_words_from_string:
-        # If the word is alpha numeric
-        if a_word.isalnum():
-            if a_word.lower() in cuss_words_list:
-                list_of_words_from_string[list_of_words_from_string.index(a_word)] = convert_cuss_word_to_stars(a_word)
-            else:
-                pass
-        #if the word has some special characters.
+        if a_word.lower() in cuss_words_list:
+            list_of_words_from_string[list_of_words_from_string.index(a_word)]=convert_cuss_word_to_stars(a_word)
+
         else:
-            streak = 0
-            some_word = ""
-            for a_character in a_word:
-                if a_character.isalpha():
-                    some_word += a_character
-                    streak += 1
-                else:
-                    streak = 0
-                    some_word=""
-                if streak >1:
-                    if some_word.lower() in cuss_words_list:
-                        list_of_words_from_string[list_of_words_from_string.index(a_word)] = convert_cuss_word_to_stars(a_word)
-                    else:
-                        pass
-                else:
-                    pass
-    return ' '.join(list_of_words_from_string)
+            if check_for_special_character(a_word.lower())==True:
+                if check_if_actually_a_cussword(a_word.lower(),cuss_words_list)==True:
+                    list_of_words_from_string[list_of_words_from_string.index(a_word)] = convert_cuss_word_to_stars(a_word)
 
 
-cuss_words_list=list(Cusslist.objects.all().values_list('cussword', flat=True))
+
+
+    return " ".join(list_of_words_from_string)
+
+
+
+cuss_words_list=tuple(Cusslist.objects.all().values_list('cussword', flat=True))
 
 # Create your views here.
 def index(request):
